4828c966592c9d794368f0c8b05a1bd3daef5b23
Maxwell Lincoln <lincoln.maxwell@gmail.com>
2021-07-29 19:29:57 -0300

    Mudando a porta de escuta do server para 5001

--- code/server.js
+++ code/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app')
-const port = process.env.port || 5000;
-const server = http.createServer;
+const port = process.env.port || 5001;
+const server = http.createServer(app);
 server.listen(port);
