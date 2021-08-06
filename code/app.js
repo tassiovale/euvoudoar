9a2c66e5924335baca68722bea36ebc73db8a4f2
Higor <higor-sj@hotmail.com>
2021-08-06 18:51:06 -0300

    update json config https || update

--- code/app.js
+++ code/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use((req, res, next) => {
     res.status(200).send({
-        mensagem: 'Eu vou doar - Jenkins - PM2 WATCH - API \n Teste Final - Funcionando!'
+        mensagem: 'Eu vou doar - API'
     });
 });
 
--- code/server.js
+++ code/server.js
@@ -7,9 +7,9 @@ const app = require('./app')
 const port = 3000
 
 const httpsOptions = {
-    cert = fs.readFileSync('./ssl/euvoudoar_com_br.crt'),
-    ca = fs.readFileSync('./ssl/euvoudoar_com_br.ca-bundle'),
-    key = fs.readFileSync('./ssl/euvoudoar.key')
+    cert: fs.readFileSync('/etc/nginx/ssl/euvoudoar_com_br.crt'),
+    ca: fs.readFileSync('/etc/nginx/ssl/euvoudoar_com_br.ca-bundle'),
+    key: fs.readFileSync('/etc/nginx/ssl/euvoudoar.key')
 }
 
 // const port = process.env.port || 3000;
