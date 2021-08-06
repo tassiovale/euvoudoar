cef93ba4fc5ebc1354fbc960a0064d754b4ce16f
Ejziel <ziel_ss@hotmail.com>
2021-08-06 15:43:02 -0300

    enable https on node

--- code/server.js
+++ code/server.js
@@ -1,5 +1,17 @@
-const http = require('http');
+const fs = requiere('fs')
+const https = require('https');
+
 const app = require('./app')
-const port = process.env.port || 3000;
-const server = http.createServer(app);
+
+//const hostname = 'www.api.euvoudoar.com.br';
+const port = 3000
+
+const httpsOptions = {
+    cert = fs.readFileSync('./etc/nginx/ssl/api_euvoudoar_com_br_chain.crt'),
+    ca = fs.readFileSync('./etc/nginx/ssl/api_euvoudoar_com_br.ca-bundle'),
+    key = fs.readFileSync('./etc/nginx/ssl/api_euvoudoar.key')
+}
+
+// const port = process.env.port || 3000;
+const server = https.createServer(httpsOptions, app);
 server.listen(port);
