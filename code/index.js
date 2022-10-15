8e1d965e749d32cbd4e520246a99bcddc269fe98
Tassio Vale <tassio.vale@fpc.ufba.br>
2022-10-15 10:16:34 -0300

    criando projeto de API em Node Express

--- code/index.js
+++ code/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('API euvoudoar')
 })
 
 const port = process.env.SERVER_PORT
