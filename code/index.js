ba0596dd3d3e522eda8ddec9897333bb89749338
Tassio Vale <tassio.vale@fpc.ufba.br>
2022-10-15 10:26:37 -0300

    substituindo require por import no index.js

--- code/index.js
+++ code/index.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
-const express = require('express')
+import dotenv from 'dotenv'
+import express from 'express'
 
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
