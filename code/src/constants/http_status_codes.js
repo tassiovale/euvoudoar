d8012b57627b321a59da86440b73171f411faff4
Tassio Vale <tassio.vale@fpc.ufba.br>
2022-10-22 10:30:44 -0300

    implementando rotas do recurso users com arrays e lodash

---
+++ code/src/constants/http_status_codes.js
@@ -0,0 +1,9 @@
+export const HTTP_STATUS_OK = 200
+
+export const HTTP_STATUS_CREATED = 201
+
+export const HTTP_STATUS_BAD_REQUEST = 400
+
+export const HTTP_STATUS_NOT_FOUND = 404
+
+export const HTTP_STATUS_SERVER_ERROR = 500
