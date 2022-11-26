1923ece597f64c623aff62f776bc42f11c8eceea
crocha12 <camilorocha11@gmail.com>
2022-11-25 23:02:40 -0300

    test: test new return institution finded by id

--- code/src/routes/institutions/__test__/findInstituitionById.test.js
+++ code/src/routes/institutions/__test__/findInstituitionById.test.js
@@ -47,6 +47,15 @@ describe('GET /institutions/{id}', () => {
             .set('x-access-token', user.token)
             .then(res => {
                 expect(res.body.id).toEqual(institutionId)
+                expect(res.body.name).toEqual("XPTO")
+                expect(res.body.cnpj).toEqual("61.394.098/0001-01")
+                expect(res.body.paymentGateway.type).toEqual("pagarme")
+                expect(res.body.paymentGateway.apiKey).toEqual("fkldsy879ey087yfg908stfb089760r976b0v")
+                expect(res.body.description).toEqual("Descrição da instituição...")
+                expect(res.body.images).toEqual([
+                    "https://sistemas.ufrb.edu.br/sigaa/img/consolidacao/avaliacao.PNG",
+                    "https://cdn.4devs.com.br/imagens/logo_4devs.png"
+                ])
                 expect(res.status).toBe(200)
             })
     })
