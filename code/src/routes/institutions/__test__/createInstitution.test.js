1060b5eb93374f9666eedbd2dba12d6a6ac657d1
crocha12 <camilorocha11@gmail.com>
2022-11-25 22:04:53 -0300

    test: delete user and institution after test

--- code/src/routes/institutions/__test__/createInstitution.test.js
+++ code/src/routes/institutions/__test__/createInstitution.test.js
@@ -1,12 +1,13 @@
 import request from 'supertest'
-
 import { app } from '../../../../app.js'
 
-var userAuth = {}
-var intitutionId
+import { deleteInstitutionById } from '../../../db/institution.js'
+import { deleteUser } from '../../../db/user.js'
 
-beforeEach(async() => {
-    //Create or Login
+let userAuth = {}
+let institutionId
+
+beforeEach(async () => {
     const resCreateUser = await request(app).post('/users').send({
         name: "Tester User Admin",
         email: "tester_user_admin@mail.com",
@@ -16,35 +17,32 @@ beforeEach(async() => {
 })
 
 afterEach(async () => {
-    // TODO: await request(app).delete(`/institutions/${userAuth.id}`).send()
-    //await request(app).delete(`/users/${userAuth.id}`).send()
-    console.log("delete the tester user")
+    await deleteInstitutionById(institutionId)
+    await deleteUser(userAuth.id)
 });
 
 describe("POST /institutions", () => {
-    
-
-    test("Extected status: 200. Return the institution created.", async () => {
-
-        var exampleInstitution = {
-            "name": "XPTO",
-            "cnpj": "61.394.098/0001-01",
-            "paymentGateway": {
-                "type": "pagarme",
-                "apiKey": "fkldsy879ey087yfg908stfb089760r976b0v"
-            },
-            "description": "Descrição da instituição...",
-            "images": [
-                "https://sistemas.ufrb.edu.br/sigaa/img/consolidacao/avaliacao.PNG",
-                "https://cdn.4devs.com.br/imagens/logo_4devs.png"
-            ]
-        }
-
-        const respCreateInstitution = await request(app).post('/institutions')
-                                                        .set('x-access-token', userAuth.token)
-                                                        .send(exampleInstitution)
-        expect(respCreateInstitution.id != null).toEqual(true)
-        intitutionId = respCreateInstitution.id
+    test("Expect status: 200. Return the institution created.", async () => {
+        await request(app).post('/institutions')
+            .set('x-access-token', userAuth.token)
+            .send({
+                "name": "XPTO",
+                "cnpj": "61.394.098/0001-01",
+                "paymentGateway": {
+                    "type": "pagarme",
+                    "apiKey": "fkldsy879ey087yfg908stfb089760r976b0v"
+                },
+                "description": "Descrição da instituição...",
+                "images": [
+                    "https://sistemas.ufrb.edu.br/sigaa/img/consolidacao/avaliacao.PNG",
+                    "https://cdn.4devs.com.br/imagens/logo_4devs.png"
+                ]
+            })
+            .then((res) => {
+                expect(res.status).toBe(201)
+                expect(res.body.id).not.toBeFalsy()
+                institutionId = res.body.id
+            })
     })
 
 })
