0a8ce018eba55ff91aade3b57375fa25b9a21fa8
Tassio Vale <tassio.vale@fpc.ufba.br>
2022-11-28 09:41:14 -0300

    adicionando código assíncrono

--- code/app.js
+++ code/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 
 import userRoutes from './src/routes/users/index.js'
+import testRoutes from './src/routes/tests/index.js'
 import institutionRoutes from './src/routes/institutions/index.js'
 import donateRoutes from './src/routes/donates/index.js'
 import {HTTP_STATUS_UNAUTHORIZED} from './src/constants/httpStatusCodes.js'
@@ -10,6 +11,8 @@ import donationProfiles from './src/routes/donationProfiles/index.js'
 const app = express()
 
 app.use(express.json())
+
+app.use(testRoutes)
 
 app.use(userRoutes)
 
---
+++ code/src/routes/tests/index.js
@@ -0,0 +1,7 @@
+import { cepRouter } from './cep.js'
+
+const routes = [
+    cepRouter
+]
+
+export default routes
