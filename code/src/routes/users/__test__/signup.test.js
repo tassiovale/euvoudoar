bf24ea973916037454e1554949763aec0e3694dc
crocha12 <camilorocha11@gmail.com>
2022-12-16 21:00:24 -0300

    test: signup route test

--- code/src/routes/users/__test__/signup.test.js
+++ code/src/routes/users/__test__/signup.test.js
@@ -1,55 +1,63 @@
 import request from 'supertest'
 import { app } from '../../../../app.js'
-import {randomWord} from '../../../helpers/utils'
+import { randomWord } from '../../../helpers/utils'
 
-it(
-    'returns 201 on successful signup',
-    async () => {
-        return request(app)
-            .post('/users')
-            .send({
-                name: 'Tassio Valle',
-                email: randomWord(10)+'@ufrb.edu.br',
-                role: "ADMIN"
-            })
-            .expect(201)
-    }
-)
+import { deleteUser } from '../../../db/user.js';
 
-it(
-    'returns 400 for empty names',
-    async () => {
-        return request(app)
-            .post('/users')
-            .send({
-                name: '',
-                email: 'tassio.vale@ufrb.edu.br'
-            })
-            .expect(400)
-    }
-)
+describe('SignUp Tests', () => {
 
-it(
-    'returns 400 for invalid emails',
-    async () => {
-        return request(app)
-            .post('/users')
-            .send({
-                name: 'Tassio Valle',
-                email: 'tassio.vale@'
-            })
-            .expect(400)
-    }
-)
+    it(
+        'returns 201 on successful signup',
+        async () => {
+            let response = await request(app)
+                .post('/users')
+                .send({
+                    name: 'Tassio Valle',
+                    email: randomWord(10) + '@ufrb.edu.br',
+                    password: '123456A@b',
+                    passwordConfirmation: '123456A@b'
+                })
+            expect(response.status).toBe(201)
+            expect(response.body).toHaveProperty('token')
+            await deleteUser(response.body.id)
+        }
+    )
 
-it(
-    'returns 400 for incomplete request objects',
-    async () => {
-        return request(app)
-            .post('/users')
-            .send({
-                email: 'tassio.vale@'
-            })
-            .expect(400)
-    }
-)
+    it(
+        'returns 400 for empty names',
+        async () => {
+            return request(app)
+                .post('/users')
+                .send({
+                    name: '',
+                    email: 'tassio.vale@ufrb.edu.br'
+                })
+                .expect(400)
+        }
+    )
+
+    it(
+        'returns 400 for invalid emails',
+        async () => {
+            return request(app)
+                .post('/users')
+                .send({
+                    name: 'Tassio Valle',
+                    email: 'tassio.vale@'
+                })
+                .expect(400)
+        }
+    )
+
+    it(
+        'returns 400 for incomplete request objects',
+        async () => {
+            return request(app)
+                .post('/users')
+                .send({
+                    email: 'tassio.vale@'
+                })
+                .expect(400)
+        }
+    )
+});
