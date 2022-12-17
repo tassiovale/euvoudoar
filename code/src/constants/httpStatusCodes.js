ce2416c910b98315c063bf3879b9e012ed70a18a
crocha12 <camilorocha11@gmail.com>
2022-12-17 08:59:05 -0300

    test: find keywords tests

--- code/src/constants/httpStatusCodes.js
+++ code/src/constants/httpStatusCodes.js
@@ -2,6 +2,8 @@ export const HTTP_STATUS_OK = 200
 
 export const HTTP_STATUS_CREATED = 201
 
+export const HTTP_STATUS_NO_CONTENT = 204
+
 export const HTTP_STATUS_BAD_REQUEST = 400
 
 export const HTTP_STATUS_NOT_FOUND = 404
