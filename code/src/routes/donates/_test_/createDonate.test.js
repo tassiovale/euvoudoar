6cc15833bd7d76c6389805c3b9256c37ba7fe3ab
crocha12 <camilorocha11@gmail.com>
2022-12-16 22:21:47 -0300

    fix: bug in donate test

--- code/src/routes/donates/_test_/createDonate.test.js
+++ code/src/routes/donates/_test_/createDonate.test.js
@@ -1,55 +1,38 @@
 import request from 'supertest'
 
 import { app } from '../../../../app.js'
+import { HTTP_STATUS_CREATED } from '../../../constants/httpStatusCodes.js'
 import { deleteInstitutionById } from '../../../db/institution.js'
-import { deleteUser } from '../../../db/user.js'
+import { deleteUser, createUser } from '../../../db/user.js'
+import { makeToken } from '../../../helpers/makeToken.js'
+
+import { TEST_INFO } from '../../../__test__/testInfo'
 
 let userAuth = {}
 let institutionId
 
-beforeEach(async () => {
-    //Create or Login
-    const resCreateUser = await request(app).post('/users').send({
-        name: "Tester User Admin",
-        email: "tester_user_admin@mail.com",
-        role: "ADMIN"
-    })
-    userAuth = resCreateUser.body
-})
-
-afterEach(async () => {
-    await deleteInstitutionById(institutionId)
-    await deleteUser(userAuth.id)
-});
-
 describe("POST /institutions", () => {
 
+    beforeAll(async () => {
+        //Create or Login
+        TEST_INFO.testerAdminUser = await createUser(TEST_INFO.testerAdminUser)
+        TEST_INFO.testerAdminUser.token = makeToken(TEST_INFO.testerAdminUser)
+        userAuth = TEST_INFO.testerAdminUser
+    })
+
+    afterAll(async () => {
+        await deleteInstitutionById(institutionId)
+        await deleteUser(userAuth.id)
+    });
 
     test("Extected status: 200. Return the institution created.", async () => {
-
-        var exampleInstitution = {
-            "name": "XPTO",
-            "cnpj": "61.394.098/0001-01",
-            "paymentGateway": {
-                "type": "pagarme",
-                "apiKey": "fkldsy879ey087yfg908stfb089760r976b0v"
-            },
-            "description": "Descrição da instituição...",
-            "images": [
-                "https://sistemas.ufrb.edu.br/sigaa/img/consolidacao/avaliacao.PNG",
-                "https://cdn.4devs.com.br/imagens/logo_4devs.png"
-            ]
-        }
-
-        let respCreateInstitution
-        await request(app).post('/institutions')
+        let res = await request(app).post('/institutions')
             .set('x-access-token', userAuth.token)
-            .send(exampleInstitution)
-            .then(res => {
-                respCreateInstitution = res.body
-            })
-        expect(respCreateInstitution.id != null).toEqual(true)
-        institutionId = respCreateInstitution.id
+            .send(TEST_INFO.institution)
+        expect(res.body.id).toBeDefined()
+        expect(res.statusCode).toEqual(HTTP_STATUS_CREATED)
+        institutionId = res.body.id
+
     })
 
 })
