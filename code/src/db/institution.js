9bfe46fd9eedd00bec95a0f9d2d856388c605e5e
SinezioJr <51363662+SinezioJr@users.noreply.github.com>
2022-12-16 23:33:38 -0300

    feat: :sparkles:

--- code/src/db/institution.js
+++ code/src/db/institution.js
@@ -1,47 +1,127 @@
-import { getDatabaseClientInstance } from './clientInstance.js'
+import {
+  getSkipValueFromQuery,
+  getTakeValueFromQuery,
+} from "../helpers/paginationData.js";
+import { getDatabaseClientInstance } from "./clientInstance.js";
 
-const databaseClientInstance = getDatabaseClientInstance()
+const databaseClientInstance = getDatabaseClientInstance();
 
 const createInstitution = async (institution) => {
-    return databaseClientInstance.institution.create(
-        {
-            data: institution
-        }
-    )
-}
+  return databaseClientInstance.institution.create({
+    data: institution,
+  });
+};
 
 const findInstitutionByCNPJ = async (cnpj) => {
-
-    return databaseClientInstance.institution.findUnique({
-        where: {
-            cnpj
-        }
-    })
-}
+  return databaseClientInstance.institution.findUnique({
+    where: {
+      cnpj,
+    },
+  });
+};
 
 const findInstitutionById = async (id) => {
-    return databaseClientInstance.institution.findUnique({
-        where: { id }
-    })
-}
+  return databaseClientInstance.institution.findUnique({
+    where: { id },
+  });
+};
 
 const deleteInstitutionById = async (id) => {
-    return databaseClientInstance.institution.delete({
-        where: { id }
-    })
-}
+  return databaseClientInstance.institution.delete({
+    where: { id },
+  });
+};
 
-const updateInstitutionById = async(institution, id) => {
-    return await databaseClientInstance.institution.update({
-        where: { id },
-        data: institution
-    })
-}
+const updateInstitutionById = async (institution, id) => {
+  return await databaseClientInstance.institution.update({
+    where: { id },
+    data: institution,
+  });
+};
+
+const searchInstitutionByKeyword = async (where) => {
+  return databaseClientInstance.institution.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: where.keyword,
+          },
+        },
+        {
+          cnpj: {
+            contains: where.keyword,
+          },
+        },
+        {
+          createdBy: {
+            name: {
+              contains: where.keyword,
+            },
+          },
+        },
+        {
+          updatedBy: {
+            name: {
+              contains: where.keyword,
+            },
+          },
+        },
+      ],
+    },
+    orderBy: [
+      {
+        name: "asc",
+      },
+    ],
+    skip: getSkipValueFromQuery(where),
+    take: getTakeValueFromQuery(where),
+    select: {
+      id: true,
+      name: true,
+      cnpj: true,
+      paymentGateway: {
+        select: {
+          type: true,
+          apiKey: true,
+        },
+      },
+      description: true,
+      images: {
+        select: {
+          url: true,
+        },
+      },
+      createdAt: true,
+      updatedAt: true,
+      deletedAt: true,
+      createdBy: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+      updatedBy: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+      deletedBy: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+};
 
 export {
-    createInstitution,
-    findInstitutionByCNPJ,
-    findInstitutionById,
-    deleteInstitutionById,
-    updateInstitutionById
-}
+  createInstitution,
+  findInstitutionByCNPJ,
+  findInstitutionById,
+  deleteInstitutionById,
+  updateInstitutionById,
+  searchInstitutionByKeyword,
+};
